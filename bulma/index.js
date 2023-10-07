90185f635ecf86b9f51fc293dd261e4ac42435e6
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-07 20:34:40 +0000

    Replace JavaScript with TypeScript

--- bulma/index.js
+++
@@ -1,7 +0,0 @@
-import Tab from '../src/tabs/Tab.vue';
-import Tabs from '../src/bulma/Tabs.vue';
-import EnsoTabs from '../src/bulma/EnsoTabs.vue';
-
-export {
-    Tab, Tabs, EnsoTabs,
-};
--- renderless/index.js
+++
@@ -1,3 +0,0 @@
-import CoreTabs from '../src/renderless/CoreTabs.vue';
-
-export default CoreTabs;
